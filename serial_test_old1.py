e13d856446724df1105b1c482a88a48934e32bba
youk233_231 <tjm.yk0259@gmail.com>
2018-06-20 11:48:20 +0900

    2018/6/20_午前中

--- serial_test_old1.py
+++ serial_test_old1.py
@@ -5,24 +5,23 @@ import decimal
 sys.path.append("/Library/Python/2.7/site-packages")
 import serial
 ser = serial.Serial("/dev/tty.usbmodem14111", 9600)
-x = ser.readline()
 value = 0
-
 while True:
     # print(x)
-    #
+    # print(value)
+    x = ser.readline()
     if x == b'1\r\n':
         value = value + 0.5
         value = round(value, 3)
-        print(value)
         bpy.context.object.rotation_euler[2] = value
+        print(value)
         time.sleep(0.1)
 
     if x == b'2\r\n':
         value = value - 0.5
         value = round(value, 3)
-        print(value)
         bpy.context.object.rotation_euler[2] = value
+        print(value)
         time.sleep(0.1)
-
+pass
 ser.close()
