84f311c8876ae422f93649b00111094e79a2a1d6
youk233_231 <tjm.yk0259@gmail.com>
2018-06-21 15:55:53 +0900

    2018/6/21:inoデータ消失に伴う復旧

--- rotary_blender/rotary_blender.ino
+++ rotary_blender/rotary_blender.ino
@@ -1,31 +1,52 @@
-#define button_up 6
-#define button_down 7
+volatile int value = 0;
+volatile uint8_t prev = 0;
 
-int val1 = 0;
-int old_val1 = 0;
-int val2 = 0;
-int old_val2 = 0;
+#define led_1 6
+#define led_2  7
 
-void setup(){
-  pinMode(button_up, INPUT_PULLUP);
-  pinMode(button_down, INPUT);
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(led_1, OUTPUT);
+  pinMode(led_2, OUTPUT);
+
+  attachInterrupt(0, updateEncoder, CHANGE);
+  attachInterrupt(1, updateEncoder, CHANGE);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+
   Serial.begin(9600);
+}
+
+void updateEncoder()
+{
+  uint8_t a = digitalRead(2);
+  uint8_t b = digitalRead(3);
+
+  uint8_t ab = (a << 1) | b;
+  uint8_t encoded  = (prev << 2) | ab;
+
+  if(encoded == 0b1101 || encoded == 0b0100 || encoded == 0b0010 || encoded == 0b1011){
+    Serial.println("+0.5");
+    digitalWrite(led_1, HIGH);
+    digitalWrite(led_2, LOW);
+    delay(2000);
+    digitalWrite(led_1, LOW);
+    digitalWrite(led_2, LOW);
+
+  } else if(encoded == 0b1110 || encoded == 0b0111 || encoded == 0b0001 || encoded == 0b1000) {
+    Serial.println("-0.5");
+    digitalWrite(led_1, LOW);
+    digitalWrite(led_2, HIGH);
+    delay(2000);
+    digitalWrite(led_1, LOW);
+    digitalWrite(led_2, LOW);
   }
 
-  //コミット用
+  prev = ab;
+}
 
-void loop(){
-  val1 = digitalRead(button_up);
-  val2 = digitalRead(button_down);
-  if(val1 == HIGH && old_val1 == LOW){
-    Serial.println("1");
-    }else if(val1 == LOW){
-      val1 = old_val1;
-      }
-  if(val2 == HIGH && old_val2 == LOW){
-    Serial.println("2");
-    }else if(val2 == LOW){
-      val2 = old_val2;
-      }
-    delay(100);
-  }
+void loop()
+{
+}
